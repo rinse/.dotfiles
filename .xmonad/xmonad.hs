8e9f3728998a5374b31da31ca7fa69d6f6e4ad3c
rinse <rinse@neko2.net>
2019-10-28 12:49:20 +0900

    add some documents to xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,3 +1,10 @@
+{-|
+  This is xmonad.hs for me.
+  You may want to run the following after installing xmonad.
+  $ apt-get install dmenu xmobar
+  $ xdg-mime default pcmanfm.desktop inode/directory
+-}
+
 import XMonad
 import XMonad.Hooks.DynamicLog (xmobar)
 import XMonad.Hooks.EwmhDesktops (ewmh)
@@ -6,10 +13,12 @@ import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Util.EZConfig (additionalKeysP)
 
 
+-- |my layout. 5 / 8 stands for the golden ratio.
 myLayout = avoidStruts $ tiled ||| Mirror tiled ||| noBorders Full
     where
     tiled = Tall 1 (3 / 100) (5 / 8)
 
+-- |my key binds
 myKeys :: [(String, X ())]
 myKeys =
     [ ("M4-l", spawn "lxlock")
